47b923cf953b76b2bb9bb556c7e743906c930def
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2023-11-06 21:49:44 +0300

    Add create endpoint

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,5 +1,18 @@
 use axum::http;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct CreateQuote {
+    book: String,
+    quote: String,
+}
 
 pub async fn health() -> http::StatusCode {
     http::StatusCode::OK
 }
+
+pub async fn create_quote(
+    axum::Json(payload): axum::Json<CreateQuote>,) -> http::StatusCode {
+        println!("{:?}", payload);
+        http::StatusCode::CREATED
+    }
